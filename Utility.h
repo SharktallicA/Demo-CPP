8dcb5ad76bf657d69d9b922523b0f905a6ee4e5c
Khalid Ali <22660310+SharktallicA@users.noreply.github.com>
2018-10-29 11:03:58 +0000

    Added double rand methods to Utility

--- Utility.h
+++ Utility.h
@@ -28,7 +28,7 @@ private:
 
 public:
 	//Requests yes (true)/no (false) boolean input from the user
-	static bool Utility::getYesNo(void)
+	static bool getYesNo(void)
 	{
 		string strInput = "";
 		bool boolIsYes = true;
@@ -47,7 +47,7 @@ public:
 		return boolIsYes;
 	}
 	//Requests yes (true)/no (false) boolean input from the user with a given input notice message
-	static bool Utility::getYesNo(string strMessage)
+	static bool getYesNo(string strMessage)
 	{
 		string strInput = "";
 		bool boolIsYes = true;
@@ -69,14 +69,14 @@ public:
 	}
 
 	//Safely requests any string input from the user
-	static string Utility::getString(void)
+	static string getString(void)
 	{
 		string strInput = "";
 		getline(cin, strInput);
 		return strInput;
 	}
 	//Safely requests any string input from the user with a given input notice message
-	static string Utility::getString(string strMessage)
+	static string getString(string strMessage)
 	{
 		cout << strMessage;
 		string strInput = "";
@@ -85,7 +85,7 @@ public:
 	}
 
 	//Safely requests integer input from the user
-	static int Utility::getInteger(void)
+	static int getInteger(void)
 	{
 		string strInput = "";
 		int intInputAsInt = -1024;
@@ -97,7 +97,7 @@ public:
 		return intInputAsInt;
 	}
 	//Safely requests integer input from the user with a given input notice message
-	static int Utility::getInteger(string strMessage, int intMin, int intMax)
+	static int getInteger(string strMessage, int intMin, int intMax)
 	{
 		string strInput = "";
 		int intInputAsInt = -1024;
@@ -118,8 +118,8 @@ public:
 		return intInputAsInt;
 	}
 
-	//Generates a random number using 32-Bit Mersenne Twister 19937
-	static int Utility::generateNumber32(int intMin, int intMax)
+	//Generates a random integer using 32-Bit Mersenne Twister 19937
+	static int generateNumber32(int intMin, int intMax)
 	{
 		int intResult;
 		random_device randGenerator;
@@ -128,8 +128,8 @@ public:
 		intResult = distInt(mersenne);
 		return intResult;
 	}
-	//Generates a random number using 64-Bit Mersenne Twister 19937
-	static int Utility::generateNumber64(int intMin, int intMax)
+	//Generates a random integer using 64-Bit Mersenne Twister 19937
+	static int generateNumber64(int intMin, int intMax)
 	{
 		int intResult;
 		random_device randGenerator;
@@ -138,21 +138,50 @@ public:
 		intResult = distInt(mersenne);
 		return intResult;
 	}
+	//Generates a random double using 32-Bit Mersenne Twister 19937
+	static double generateNumber32(double dblMin, double dblMax)
+	{
+		double dblResult;
+		random_device randGenerator;
+		mt19937 mersenne(randGenerator());
+		uniform_real_distribution<double> distInt(dblMin, dblMax);
+		dblResult = distInt(mersenne);
+		return dblResult;
+	}
+	//Generates a random double using 64-Bit Mersenne Twister 19937
+	static double generateNumber64(double dblMin, double dblMax)
+	{
+		double dblResult;
+		random_device randGenerator;
+		mt19937_64 mersenne(randGenerator());
+		uniform_real_distribution<double> distInt(dblMin, dblMax);
+		dblResult = distInt(mersenne);
+		return dblResult;
+	}
 
 	//Clears the screen
-	static void Utility::clearScreen(void)
+	static void clearScreen(void)
 	{
 		system("cls");
 	}
 
+	//Waits for user input - good replacement for system("pause")
+	static char pause(string strMessage = "Press any key to continue...")
+	{
+		cout << strMessage;
+		char in = _getch();
+		cout << endl;
+		return in;
+	}
+
 	//Sets the C++ console window's title
-	static void Utility::setWindowTitle(string strTitle)
+	static void setWindowTitle(string strTitle)
 	{
 		SetConsoleTitle(strTitle.c_str());
 	}
 
 	//Sets the C++ console window's size
-	static void Utility::setWindowSize(unsigned int uintWidth, unsigned int uintHeight)
+	static void setWindowSize(unsigned int uintWidth, unsigned int uintHeight)
 	{
 		//purpose: sets the window size
 		//parametres: (uintWidth) specified window width, (uintHeight) specified window height
@@ -164,8 +193,8 @@ public:
 		MoveWindow(console, rectWindow.left, rectWindow.top, uintWidth, uintHeight, TRUE);
 	}
 
-	//Sets the C++ console window's colours
-	static void Utility::setColour(Colour clrFore = WHITE, Colour clrBack = BLACK)
+	//Sets the C++ console window's colours | Possible: BLACK, BLUE, GREEN, AQUA, RED, PURPLE, YELLOW, DEFAULT, GREY, LIGHT_BLUE, LIGHT_GREEN, LIGHT_AQUA, LIGHT_RED, LIGHT_PURPLE, LIGHT_YELLOW, WHITE
+	static void setColour(Colour clrFore = WHITE, Colour clrBack = BLACK)
 	{
 		//purpose: sets the console colour
 		//parametres: (clrFore) enumeration for foreground colour, (clrBack) enumeration for background colour (defaulted as black)
@@ -177,7 +206,7 @@ public:
 	}
 
 	//Moves the C++ console window's cursor
-	static void Utility::moveCursor(SHORT shrtX, SHORT shrtY)
+	static void moveCursor(SHORT shrtX, SHORT shrtY)
 	{
 		HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
 		COORD point = { shrtX, shrtY };
